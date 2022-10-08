028f03db37dcd303f5782c22d7743051a1c39cb2
ebonitesh0ut <ebonitesh0ut@DESKTOP-OHTQSP9>
2022-10-09 00:42:47 +0300

    Ready to use. Code is needed to be cleaned from crap

--- src/Main.java
+++ src/Main.java
@@ -24,50 +24,84 @@ public class Main {
             if (SBuf.isEmpty()) throw new Exception("Template is empty");
 
             StringBuffer SBBuf = new StringBuffer(SBuf);
-            ArrayList<String> bracketId = new ArrayList<String>();
+
+
+            ArrayList<String> lBracket = new ArrayList<String>();
+            ArrayList<String> rBracket = new ArrayList<String>();
+            ArrayList<String> bracketQueue = new ArrayList<String>();
+            ArrayList<String> bracketQueueIndex = new ArrayList<String>();
+
             int pos = 0;
             while (SBBuf.indexOf("\"") != -1){
                 pos = SBBuf.indexOf("\"");
                 SBBuf = SBBuf.delete(pos, pos+1);
             }
             System.out.println(SBBuf);
-            while(SBBuf.indexOf(",")!=-1)
+            while(SBBuf.indexOf("left")!=-1)
                 {                                             //Parsing brackets and creating variables for them
-                    bracketId.add(SBBuf.substring(0,2));
-                    SBBuf.delete(0,3);
+                    lBracket.add(SBBuf.substring(SBBuf.indexOf("left")+7,SBBuf.indexOf("left")+8));
+                    rBracket.add(SBBuf.substring(SBBuf.indexOf("right")+8, SBBuf.indexOf("right")+9));
+                    SBBuf.delete(0,SBBuf.indexOf("right")+8);
 
                 }
-            bracketId.add(SBBuf.substring(0,2));
-            SBBuf.delete(0,3);
-            System.out.println(bracketId.toString());
+
+            System.out.println(lBracket.toString());
+            System.out.println(rBracket.toString());
             scan.close();
             reader.close();
 
             try (FileReader readerText = new FileReader("src/text.txt")){
-                Scanner scanText = new Scanner(reader);
-                String SBufer = new String();
-                while (scan.hasNext()) {
-                    SBuf += scan.next();
+                Scanner scanText = new Scanner(readerText);
+                SBuf = new String();
+                while (scanText.hasNext()) {
+                    SBuf += scanText.next();
                 }
                 if (SBuf.isEmpty()) throw new Exception("Text is empty");
                 StringBuffer SBBufer = new StringBuffer(SBuf);
-                for (int i = 0; i < SBBuf.length(); i++){
-                    for (String value: bracketId) {
-                        int index = 0;
-                        if(value.indexOf(SBBufer.charAt(i))==0)
+                for (int i = 0; i < SBBufer.length(); i++) {
+                        /*if(rBracket.indexOf(SBBufer.charAt(i)) != -1)
+                        {
 
 
+                        }else if(lBracket.indexOf(SBBufer.charAt(i)) != -1)
+                        {
+                            bracketQueue.add(String.valueOf(SBBufer.charAt(i)));
+                        }*/
+
+                    if((rBracket.indexOf(String.valueOf(String.valueOf(SBBufer.charAt(i)))) != -1) && (lBracket.indexOf(String.valueOf(SBBufer.charAt(i))) == -1))
+                    {
+                        if(bracketQueue.isEmpty()){
+                            bracketQueueIndex.add(String.valueOf(i));
+                        }else if(lBracket.indexOf(bracketQueue.get(bracketQueue.size()-1)) == rBracket.indexOf(String.valueOf(SBBufer.charAt(i)))){
+                                bracketQueue.remove(bracketQueue.size()-1);
+                            }else{
+                                //bracketQueue.remove(bracketQueue.size()-1);
+                                bracketQueueIndex.add(String.valueOf(i));
+                            }
+
+                        /*bracketQueue.add(String.valueOf(SBBufer.charAt(i)));
+                        bracketQueueIndex.add(String.valueOf(i));*/
+                    }else if((rBracket.indexOf(String.valueOf(SBBufer.charAt(i))) != -1) && (lBracket.indexOf(String.valueOf(SBBufer.charAt(i))) != -1)){
+                        if(bracketQueue.isEmpty()){
+                            bracketQueue.add(String.valueOf(SBBufer.charAt(i)));
+                        }else if((lBracket.indexOf(bracketQueue.get(bracketQueue.size()-1))) == (rBracket.indexOf(String.valueOf(SBBufer.charAt(i))))){
+                            bracketQueue.remove(bracketQueue.size()-1);
+                        }else {
+                            bracketQueue.add(String.valueOf(SBBufer.charAt(i)));
+                            /*System.out.println((lBracket.indexOf(bracketQueue.get(bracketQueue.size()-1))));
+                            System.out.println(SBBufer.charAt(i));
+                            System.out.println((rBracket.indexOf(SBBufer.charAt(i))));*/
+                        }
+                    }else if((lBracket.indexOf(String.valueOf(SBBufer.charAt(i))) != -1)){
+                        bracketQueue.add(String.valueOf(SBBufer.charAt(i)));
                     }
-                    }
-            
-            
-            
-            
-            
+                }
+                if(bracketQueueIndex.isEmpty())
+                    System.out.println("No mistakes found");
+                else
+                    System.out.println("Found mistakes on position(s): " + bracketQueueIndex.toString());
         }
         catch(Exception e) {throw new Exception("Error while parsing template");}
-
-        
         }
 
     }
